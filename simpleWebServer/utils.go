4c82b503c8af2dffcfa7e4024d6bec4f620ec8ce
Suaralanre <sirstuffy@gmail.com>
2024-12-14 18:48:24 +0100

    Server gracefully shutdown

--- simpleWebServer/utils.go
+++ simpleWebServer/utils.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"encoding/json"
+	"fmt"
 	"net/http"
 )
-
-type ErrorResponse struct {
-	Error string `json:"error"`
-}
 
 func WriteError(w http.ResponseWriter, message string, statusCode int) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
-	res := ErrorResponse{Error: message}
-	json.NewEncoder(w).Encode(res)
+	fmt.Fprintf(w, "%s", message)
 }
